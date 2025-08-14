87c0b29d98bb6c7d1874e593bccdca9e488f446d
Serhat <s.ciftci561@gmail.com>
2025-08-15 00:07:51 +0300

    IRepsitoryProduct ve ProductRepository oluşturuldu.

---
+++ Product.Repositories/Repo/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Product.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Repositories.Repo
+{
+    public interface IProductRepository
+    {
+        //bütün ürünleri getirir.
+        Task<List<ProductModel>> GetAllProductAsync();
+        // idye gören ürünümüzü getirir.
+        Task<ProductModel?> GetProductGetByIdAsync(int id);
+        //ekleme
+        Task<ProductModel> AddProductAsync(ProductModel product);
+        //güncelleme 
+        Task<ProductModel> UpdateProductAsync(ProductModel product);
+        //slilme
+        Task DeleteProductAsync(int id);
+    }
+}
---
+++ Product.Repositories/Repo/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Repositories.Context;
+using Product.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Repositories.Repo
+{
+    public class ProductRepository : IProductRepository
+    {
+        //Dependy Injection
+        private readonly AppDbContext _context;
+        public ProductRepository(AppDbContext context)
+        {
+            _context= context; 
+        }
+        public async Task<ProductModel> AddProductAsync(ProductModel product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x=> x.Id == id);
+            if (product != null) 
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<ProductModel>> GetAllProductAsync()
+        {
+            var result = await _context.Products.ToListAsync();
+            return result;
+        }
+
+        public async Task<ProductModel?> GetProductGetByIdAsync(int id)
+        {
+            var product =await _context.Products.FindAsync(id);
+            return product;
+
+        }
+
+        public async Task<ProductModel> UpdateProductAsync(ProductModel product)
+        {
+           _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
