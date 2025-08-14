f921fb268a2dc2a79c0eb05805f358af2d0024e7
Serhat <s.ciftci561@gmail.com>
2025-08-14 16:27:34 +0300

    Add project files.

---
+++ Product.Repositories/Models/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Repositories.Models
+{
+
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+
+        public string ProductName { get; set; } = string.Empty;
+
+
+        public string ProductDescription { get; set; } = string.Empty;
+
+
+        public decimal ProductPrice { get; set; }
+
+        public int ProductStock { get; set; }
+
+    }
+}
